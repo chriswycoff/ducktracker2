ce3bb7573842abce032d31f4c129ad49f41c6474
Christopher Wycoff <christopherwycoff@Christophers-MacBook-Pro-7.local>
2020-05-31 16:00:27 -0700

    added some bash skeleton

--- for_ix/duck_tracker_server/server_setup.sh
+++ for_ix/duck_tracker_server/server_setup.sh
@@ -7,5 +7,20 @@ echo "USER" "$dbuser" "PASSWORD" "$password"
 
 mysql -p -e "grant all privileges on *.* to '$dbuser'@'%' identified by '$password' ;" 
 
+mysql -u"$dbuser" -p"$password" -e "
+CREATE DATABASE $database;
+show databases;
+use $database;
+
+CREATE TABLE $table (
+ID VARCHAR(255),
+time VARCHAR(255),
+latitude VARCHAR(255),
+longitude VARCHAR(255),
+tal VARCHAR(255),
+date VARCHAR(255)
+);
+SHOW TABLES;" 
+
 
 # mysql 
