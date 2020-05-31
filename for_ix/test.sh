bda2557f61ca9b762618dc2d395b01e2317ba557
Christopher Wycoff <christopherwycoff@Christophers-MacBook-Pro-7.local>
2020-05-31 11:40:49 -0700

    added some bash skeleton

---
+++ for_ix/test.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+# Optionally, set default values
+# var1="default value for var1"
+# var1="default value for var2"
+. test.ini
+
+echo "$var1" "$var2"
+
+
+echo "USER" "$dbuser" "PASSWORD" "$password" 
